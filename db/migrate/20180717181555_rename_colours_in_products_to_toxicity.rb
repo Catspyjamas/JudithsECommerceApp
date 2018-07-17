46f4f86ba7aa6f580307f2e47e2309839d256505
Catspyjamas <judithurban@gmx.net>
2018-07-18 01:22:03 +0200

    4.2 Solved a Heroku Problem: Force Push

--- db/migrate/20180717181555_rename_colours_in_products_to_toxicity.rb
+++ db/migrate/20180717181555_rename_colours_in_products_to_toxicity.rb
@@ -1,5 +1,5 @@
 class RenameColoursInProductsToToxicity < ActiveRecord::Migration[5.2]
   def change
-    rename_column :products, :color, :toxicity
+    rename_column :products, :colour, :toxicity
   end
 end
