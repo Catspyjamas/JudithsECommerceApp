50ef99460a09720cdc40e41fa174ad73278483f2
Catspyjamas <judithurban@gmx.net>
2018-08-20 10:30:44 +0200

    6.5 Review: Remove byebug line

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,7 +20,6 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    byebug
     @product = Product.find(params[:id])
     @comments = @product.comments.paginate(:page => params[:page], :per_page => 5).order('created_at DESC')
   end
